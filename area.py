07d54e12a0af6f20533cc69cbd097192eab25780
Akshithatirmal <113929658+Akshithatirmal@users.noreply.github.com>
2023-06-23 18:53:25 +0530

    Add files via upload
    1.Python program which accepts the radius of a circle from the user and computes area.
    2.Python program to accept a filename from the user and print the extension of that.

---
+++ area.py
@@ -0,0 +1,6 @@
+import math
+def area(Radius):
+    area = Radius** 2 * math.pi
+    return area
+Radius = float (input (""))
+print (area(Radius))
---
+++ files.py
@@ -0,0 +1,5 @@
+f= input("Enter Filename: ")
+
+f1= f.split(".")
+
+print ("Extension of the file is : " + f1[-1])
